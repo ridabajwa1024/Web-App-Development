ed0605ec9db13ab0523d1be44fa41dc1861ce5b4
RidaBajwa <ridabajwa1024@gmail.com>
2025-07-09 18:57:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// export const student={
+// Name:"rida",
+// Age: 20,
+// Class:12,
+// Marks : 97,
+// }
+
+export const mathFunctions={
+Sum:function(Number1,Number2){
+return Number1 + Number2
+},
+Multiply:function(Number1,Number2){
+return Number1 * Number2
+}
+}
---
+++ script2.js
@@ -0,0 +1,7 @@
+// import{student} from './script.js'
+// console.log(student)
+
+import{mathFunctions} from './script.js';
+console.log(mathFunctions.Sum(14,9))
+console.log(mathFunctions.Multiply(54,9))
+
